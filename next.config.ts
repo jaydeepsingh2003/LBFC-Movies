fc173aad87b16e2493792cb6b742c62cc930199a
Jaydeep Singh Galhotra <jaydeepeingh8774@gmail.com>
2025-11-29 12:45:54 +0000

    Option A: Using next.config.js (For Next.js Projects) If your project is

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/tmdb/:path*',
+        destination: `https://api.themoviedb.org/:path*`,
+      },
+    ]
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
--- src/lib/tmdb.client.ts
+++ src/lib/tmdb.client.ts
@@ -2,9 +2,8 @@
 
 import { Movie } from "./tmdb";
 
-const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
-const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 const TMDB_API_KEY = "2dc0bd12c7bd63b2c691d3a64f3a3db7";
+const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 
 export async function searchMovies(query: string): Promise<Movie[]> {
@@ -13,20 +12,18 @@ export async function searchMovies(query: string): Promise<Movie[]> {
     return [];
   }
 
-  const url = `${TMDB_API_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`;
+  const url = `/api/tmdb/3/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`;
   
   try {
     const response = await fetch(url);
-
     if (!response.ok) {
       console.error(`TMDB API request failed with status ${response.status}`);
       return [];
     }
-
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error('Error fetching from TMDB API:', error);
+    console.error('Error fetching from TMDB API via proxy:', error);
     return [];
   }
 }
