9bfcd7a02dac45a1ba6719d74e5a0996e86dc2d2
Jaydeep Singh Galhotra <jaydeepeingh8774@gmail.com>
2025-11-30 06:43:30 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCNBeB_GTcrVzUbzAv7B_Qajtf_jUQ8

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,12 @@
 import { FirebaseOptions } from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-  "apiKey": "YOUR_API_KEY",
-  "authDomain": "YOUR_AUTH_DOMAIN",
-  "projectId": "YOUR_PROJECT_ID",
-  "storageBucket": "YOUR_STORAGE_BUCKET",
-  "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
-  "appId": "YOUR_APP_ID",
-  "measurementId": "YOUR_MEASUREMENT_ID"
+  "apiKey": "AIzaSyCNBeB_GTcrVzUbzAv7B_Qajtf_jUQ8kcs",
+  "authDomain": "studio-4492377973-e38dd.firebaseapp.com",
+  "projectId": "studio-4492377973-e38dd",
+  "storageBucket": "studio-4492377973-e38dd.appspot.com",
+  "messagingSenderId": "218914691188",
+  "appId": "1:218914691188:web:cc570f1a6d3d2d87409921"
 };
 
 export default firebaseConfig;
