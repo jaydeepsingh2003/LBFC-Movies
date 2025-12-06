7106000bc6f31a321ca491938eff6efe258c1f29
Jaydeep Singh Galhotra <jaydeepeingh8774@gmail.com>
2025-12-06 14:23:04 +0000

    'use client';
    import { Header } from './header';
    import { useUser } fro

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -5,7 +5,6 @@ import { Header } from './header';
 import { useUser } from '@/firebase/auth/auth-client';
 import { BottomNav } from './bottom-nav';
 import { useIsMobile } from '@/hooks/use-mobile';
-import { SidebarProvider } from '../ui/sidebar';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   const { user } = useUser();
