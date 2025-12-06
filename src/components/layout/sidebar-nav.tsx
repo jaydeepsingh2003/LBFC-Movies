5851ed84e2b4c9f59308186d3815427b30cd6f84
Jaydeep Singh Galhotra <jaydeepeingh8774@gmail.com>
2025-12-06 05:36:51 +0000

    ADD all the section of the TMDB Features without leaving one of the feat

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -11,4 +11,5 @@ export const navItems = [
     { href: "/playlists", label: "Playlists", icon: ListVideo },
     { href: "/watch-parties", label: "Parties", icon: Tv },
     { href: "/social", label: "Social", icon: Users },
+    { href: "/news", label: "News", icon: Newspaper },
 ];
