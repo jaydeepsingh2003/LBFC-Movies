2560b3a915314ace4c0ee27e491897b4f3b96d73
Jaydeep Singh Galhotra <jaydeepeingh8774@gmail.com>
2025-12-06 14:14:14 +0000

    fill the right side black area to fit the all content correctly by remov

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -14,7 +14,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className={`flex flex-1 flex-col w-full pb-16 md:pb-0`}>
+      <div className="flex-1">
         <main className="flex-1 animate-in fade-in duration-500">
           {children}
         </main>
